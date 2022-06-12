235cbb331662733fc2e5e0201cbd5ee791c171cd
Mat Jordan <mat@northwestern.edu>
2022-06-12 08:50:18 -0400

    Organize fixtures.

---
+++ src/fixtures/collection/collection-no-thumbs.ts
@@ -0,0 +1,79 @@
+export const items = [
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon.json",
+    type: "Manifest",
+    label: {
+      none: ["Half Moon - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/02-lawyer.json",
+    type: "Manifest",
+    label: {
+      none: ["Lawyer - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/03-matron.json",
+    type: "Manifest",
+    label: {
+      none: ["Nez Percé Matron"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/04-three-eagles.json",
+    type: "Manifest",
+    label: {
+      none: ["Three Eagles - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/05-raven-blanket.json",
+    type: "Manifest",
+    label: {
+      none: ["Raven Blanket - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/06-night-scout.json",
+    type: "Manifest",
+    label: {
+      none: ["Night Scout - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/07-grizzly-bear-ferocious.json",
+    type: "Manifest",
+    label: {
+      none: ["Grizzly-bear Ferocious - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/08-map-territory.json",
+    type: "Manifest",
+    label: {
+      none: ["Map of Nez Percé Territory"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/09-girl.json",
+    type: "Manifest",
+    label: {
+      none: ["Nez Percé Girl"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/10-kulkultsalum.json",
+    type: "Manifest",
+    label: {
+      none: ["Kulkultsálum - Nez Percé"],
+    },
+  },
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/11-watching-for-the-signal.json",
+    type: "Manifest",
+    label: {
+      none: ["Watching for the Signal - Nez Percé"],
+    },
+  },
+];
---
+++ src/fixtures/manifest/manifest-short.ts
@@ -0,0 +1,29 @@
+export const items = [
+  {
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0",
+    type: "AnnotationPage",
+    items: [
+      {
+        id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0/annotation/0",
+        type: "Annotation",
+        motivation: "painting",
+        target:
+          "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0",
+        body: {
+          id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d/full/600,/0/default.jpg",
+          type: "Image",
+          format: "image/jpeg",
+          service: [
+            {
+              id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d",
+              profile: "http://iiif.io/api/image/2/level2.json",
+              type: "ImageService2",
+            },
+          ],
+          width: 2161,
+          height: 3072,
+        },
+      },
+    ],
+  },
+];

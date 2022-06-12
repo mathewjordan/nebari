a070354916fdf75b033bb2a6107307e73a1dcb1a
Mat Jordan <mat@northwestern.edu>
2022-06-12 08:53:20 -0400

    Fix half moon, add simple image manifest.

--- src/fixtures/manifest/manifest-short.ts
+++ src/fixtures/manifest/manifest-short.ts
@@ -1,28 +1,39 @@
 export const items = [
   {
-    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0",
-    type: "AnnotationPage",
+    id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0",
+    type: "Canvas",
+    width: 2161,
+    height: 3072,
+    label: {
+      en: ["Half Moon - Nez Percé"],
+    },
     items: [
       {
-        id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0/annotation/0",
-        type: "Annotation",
-        motivation: "painting",
-        target:
-          "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0",
-        body: {
-          id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d/full/600,/0/default.jpg",
-          type: "Image",
-          format: "image/jpeg",
-          service: [
-            {
-              id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d",
-              profile: "http://iiif.io/api/image/2/level2.json",
-              type: "ImageService2",
+        id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0",
+        type: "AnnotationPage",
+        items: [
+          {
+            id: "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0/page/0/annotation/0",
+            type: "Annotation",
+            motivation: "painting",
+            target:
+              "https://canopy-iiif.vercel.app/iiif/manifest/nez-perce/01-half-moon/canvas/0",
+            body: {
+              id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d/full/600,/0/default.jpg",
+              type: "Image",
+              format: "image/jpeg",
+              service: [
+                {
+                  id: "https://iiif.stack.rdc.library.northwestern.edu/iiif/2/1ce40089-9317-4dc7-823a-3af757d55c5d",
+                  profile: "http://iiif.io/api/image/2/level2.json",
+                  type: "ImageService2",
+                },
+              ],
+              width: 2161,
+              height: 3072,
             },
-          ],
-          width: 2161,
-          height: 3072,
-        },
+          },
+        ],
       },
     ],
   },

e24159a6ef496c239df7cebde1b322bdd33b7cbd
Mat Jordan <mat@northwestern.edu>
2022-06-21 09:27:50 -0400

    Rename static title.

--- utils/html.js
+++ utils/html.js
@@ -6,7 +6,7 @@ async function buildHTML(isStatic) {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Nectar IIIF</title>
+      <title>Nebari, an experiment in items</title>
       ${isStatic ? ` <link rel="stylesheet" href="./default.css" />` : ``}
       <link rel="stylesheet" href="./styles.css" />
     </head>
